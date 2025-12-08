c0e01fe0d2e6ae4dcd27f09e67108dbeb0b31c21
mkeisyafs <mobilezero24@gmail.com>
2025-12-08 09:05:31 +0700

    route + vercel

--- src/constant/router.tsx
+++ src/constant/router.tsx
@@ -1,56 +1,60 @@
-import { createBrowserRouter } from 'react-router-dom';
-import Layout from '../components/layout';
-import { listed } from './listed';
-import Dashboard from '../pages/dashboard';
-import LandingPage from '../pages/landing';
-import Produk from '../pages/produk';
-import Transaksi from '../pages/transaksi';
-import Auth from '../pages/auth';
-import AuthProtectLayout from '../components/AuthProtectLayout';
-import Profile from '../pages/profile';
-import ResetPassword from '../pages/resetpassword';
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "../components/layout";
+import { listed } from "./listed";
+import Dashboard from "../pages/dashboard";
+import LandingPage from "../pages/landing";
+import Produk from "../pages/produk";
+import Transaksi from "../pages/transaksi";
+import Auth from "../pages/auth";
+import AuthProtectLayout from "../components/AuthProtectLayout";
+import Profile from "../pages/profile";
+import ResetPassword from "../pages/resetpassword";
 
 export const Router = createBrowserRouter([
-    {
-    path: '/',
+  {
+    index: true,
+    element: <LandingPage />,
+  },
+  {
+    path: "/",
     element: (
-        <AuthProtectLayout>
-            <Layout />
-        </AuthProtectLayout>
+      <AuthProtectLayout>
+        <Layout />
+      </AuthProtectLayout>
     ),
     children: [
-        {
-            index: true,
-            element: <Dashboard />,
-        },
-        {
-            path: listed.dashboard,
-            element: <Dashboard />
-        },
-        {
-            path: listed.transaksi,
-            element: <Transaksi />
-        },
-        {
-            path: listed.produk,
-            element: <Produk />
-        },
-        {
-            path: listed.profile,
-            element: <Profile />
-        }
+      {
+        index: true,
+        element: <Dashboard />,
+      },
+      {
+        path: listed.dashboard,
+        element: <Dashboard />,
+      },
+      {
+        path: listed.transaksi,
+        element: <Transaksi />,
+      },
+      {
+        path: listed.produk,
+        element: <Produk />,
+      },
+      {
+        path: listed.profile,
+        element: <Profile />,
+      },
     ],
-    },
-    {
-        path: listed.landingPage,
-        element: <LandingPage />
-    },
-    {
-        path: listed.auth,
-        element: <Auth />
-    },
-    {
-        path: listed.resetPassword,
-        element: <ResetPassword />
-    }
-])
+  },
+  {
+    path: listed.landingPage,
+    element: <LandingPage />,
+  },
+  {
+    path: listed.auth,
+    element: <Auth />,
+  },
+  {
+    path: listed.resetPassword,
+    element: <ResetPassword />,
+  },
+]);
